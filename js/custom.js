34492811e843e10214d4e492b37bda4e2ddd3f07
my work <hdabhi287@gmail.com>
2024-06-12 03:42:00 -0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+//
+//	Equal height - by Lewi Hussey
+//
+
+var matchHeight = function () {
+	
+	function init() {
+		eventListeners();
+		matchHeight();
+	}
+	
+	function eventListeners(){
+		$(window).on('resize', function() {
+			matchHeight();
+		});
+	}
+	
+	function matchHeight(){
+		var groupName = $('[data-match-height]');
+		var groupHeights = [];
+		
+		groupName.css('min-height', 'auto');
+		
+		groupName.each(function() {
+			groupHeights.push($(this).outerHeight());
+		});
+		
+		var maxHeight = Math.max.apply(null, groupHeights);
+		groupName.css('min-height', maxHeight);
+	};
+	
+	return {
+		init: init
+	};
+	
+} ();
+
+$(document).ready(function() {
+	matchHeight.init();
+});
